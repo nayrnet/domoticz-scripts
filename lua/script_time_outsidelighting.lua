78f973a6be366aa806302e134a6cabc09c88a78b
Ryan Hunt <admin@nayr.net>
2016-01-29 23:21:11 -0700

    cleanup

--- lua/script_time_outsidelighting.lua
+++ lua/script_time_outsidelighting.lua
@@ -4,16 +4,16 @@ time = os.date("*t")
 libs = require("libs")	-- Include common functions
 
 -- Front Door: Shut off if on for more than 15mins after 10pm. Otherwise force it on in evening.
-if (otherdevices['Front Door Light'] ~= 'Off') and (libs.timedifference(otherdevices_lastupdate['Front Door Light']) >= 900) and 
-	((time.hour >= 22) and (time.hour < 16)) then
+if (otherdevices['Front Door Light'] ~= 'Off') and (libs.timedifference(otherdevices_lastupdate['Front Door Light']) >= 900)
+	and ((time.hour >= 22) and (time.hour < 16)) then
 		commandArray['Front Door Light']='Off' 
 elseif (otherdevices['Front Door Light'] == 'Off') and (time.hour < 22) and (timeofday['Nighttime']) then
 		commandArray['Front Door Light']='On'
 end
 
 -- Front Security Lighting: Shut off if on for more than 60mins after Midnight once the TV is Off. Otherwise force it on in evening. 
-if (otherdevices['Front Security Lights'] ~= 'Off') and (libs.timedifference(otherdevices_lastupdate['Front Security Lights']) >= 3600) and 
-	((time.hour >= 0) and (time.hour < 16)) and (otherdevices['TV'] == 'Off') then
+if (otherdevices['Front Security Lights'] ~= 'Off') and (libs.timedifference(otherdevices_lastupdate['Front Security Lights']) >= 3600)
+	and ((time.hour >= 0) and (time.hour < 16)) and (otherdevices['TV'] == 'Off') then
 		commandArray['Front Security Lights']='Off'
 elseif (otherdevices['Front Door Light'] == 'Off') and (time.hour <= 23) and (timeofday['Nighttime']) then
 		commandArray['Front Security Lights']='On'
