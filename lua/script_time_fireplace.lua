9536338c39b14a68fd7f7ff66a9b674e18cfad57
Ryan Hunt <admin@nayr.net>
2016-02-12 14:03:20 -0700

    fireplace script needed C not F

--- lua/script_time_fireplace.lua
+++ lua/script_time_fireplace.lua
@@ -5,15 +5,15 @@ time = os.date("*t")
 -- Fireplace: Shut off after 10pm once the TV turns off, otherwise turn on after 7am if under 50F
 if (otherdevices['Fireplace'] ~= 'Off') and ((time.hour >= 22) or (time.hour < 7)) and (otherdevices['TV'] == 'Off') then
 		commandArray['Fireplace']='Off' 
-elseif (otherdevices['Fireplace'] == 'Off') and (time.hour < 22) and (time.hour > 6) and (otherdevices_temperature['Online Weather'] < 50) then
+elseif (otherdevices['Fireplace'] == 'Off') and (time.hour < 22) and (time.hour > 6) and (otherdevices_temperature['Online Weather'] < 10) then
 	if (tonumber(uservariables["away"]) < 1) then
 		commandArray['Fireplace']='On'
 		print('Its cold outside, turning on fireplace..')
 	end
 end
 
--- Fireplace: Shutoff if its warmer than 60F outside
-if (otherdevices['Fireplace'] ~= 'Off') and (otherdevices_temperature['Online Weather'] > 60) then
+-- Fireplace: Shutoff if its warmer than 55F outside
+if (otherdevices['Fireplace'] ~= 'Off') and (otherdevices_temperature['Online Weather'] > 13) then
 		commandArray['Fireplace']='Off' 
 		print('Too warm outside for fireplace, shutting off..')
 end
--- lua/script_time_outsidelighting.lua
+++ lua/script_time_outsidelighting.lua
@@ -1,4 +1,6 @@
--- Time script for Outside Lighting, these are turned on automatically with ZW groups so we will handle that here.
+-- Time script for Outside Lighting, they have onboard light sensors and brighten with motion.
+-- They are very sensitive so I shut them off late at night unless something physical happens.
+
 commandArray = {}
 time = os.date("*t")
 libs = require("libs")	-- Include common functions
@@ -10,6 +12,14 @@ if (otherdevices['Front Door Light'] ~= 'Off') and (libs.timedifference(otherdev
 elseif (otherdevices['Front Door Light'] == 'Off') and (time.hour < 22) and (time.hour > 12) and (timeofday['Nighttime']) then
 		commandArray['Front Door Light']='On'
 end
+
+-- Back Door: Shut off if on for more than 30mins after 10pm. Otherwise force it on in evening.
+-- if (otherdevices['Back Door Light'] ~= 'Off') and (libs.timedifference(otherdevices_lastupdate['Back Door Light']) >= 1800)
+--	and ((time.hour >= 22) or (time.hour < 12)) then
+--		commandArray['Back Door Light']='Off' 
+-- elseif (otherdevices['Back Door Light'] == 'Off') and (time.hour < 22) and (time.hour > 12) and (timeofday['Nighttime']) then
+--		commandArray['Back Door Light']='On'
+-- end
 
 -- Front Security Lighting: Shut off if on for more than 60mins after Midnight once the TV is Off. Otherwise force it on in evening. 
 if (otherdevices['Front Security Lights'] ~= 'Off') and (libs.timedifference(otherdevices_lastupdate['Front Security Lights']) >= 3600)
