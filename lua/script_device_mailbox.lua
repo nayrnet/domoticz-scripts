366e84ae07addcee1acb057835d75882c385183d
Ryan Hunt <admin@nayr.net>
2016-02-12 23:22:40 -0700

    fix mailbox

--- lua/script_device_mailbox.lua
+++ lua/script_device_mailbox.lua
@@ -1,13 +1,13 @@
 -- Send notification when mail has arrived, newmail is reset with script_time_mailbox.lua
 commandArray = {}
-if (devicechanged['Mailbox'] == 'Open') and (tonumber(uservariables["newmail"]) == 0) then
+if (devicechanged['Mailbox'] == 'On') and (tonumber(uservariables["newmail"]) == 0) then
 	commandArray['Variable:newmail']="1"
 	print("Today's Mail has arrived.")
 	commandArray['SendNotification']='Mail Dispatch#Todays post was just delivered.#0'
-	os.execute('ogg123 -q ~/audio/newmail.ogg')
-elseif (devicechanged['Mailbox'] == 'Open') and (tonumber(uservariables["newmail"]) == 1) then
+	os.execute('ogg123 -q ~/audio/newmail.ogg &')
+elseif (devicechanged['Mailbox'] == 'On') and (tonumber(uservariables["newmail"]) == 1) then
 	print("Mailbox was just accessed.")
 	commandArray['SendNotification']='Perimeter Alert#The Mailbox was just accessed.#0'
-	os.execute('ogg123 -q ~/audio/mailopen.ogg')
+	os.execute('ogg123 -q ~/audio/mailopen.ogg &')
 end
 return commandArray
