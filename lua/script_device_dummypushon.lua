f138f4c8c52d7d6840aa0ab1df0aaf6096928316
Ryan Hunt <admin@nayr.net>
2016-02-15 02:31:58 -0700

    bunch of updates

--- lua/script_device_dummypushon.lua
+++ lua/script_device_dummypushon.lua
@@ -1,8 +1,8 @@
 -- turn dummy push-on switches back off
 
 commandArray = {}
-if (devicechanged['West PTZ Idle (Update)'] == 'On') then
-        commandArray['West PTZ Idle (Update)']='Off AFTER 5'
+if (devicechanged['West PTZ Car Location'] == 'On') then
+        commandArray['West PTZ Car Location']='Off AFTER 5'
 end
 
 return commandArray
