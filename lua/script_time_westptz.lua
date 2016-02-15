f07312e2bb8e8b31efbee858fb239dbc65547097
Ryan Hunt <admin@nayr.net>
2016-02-15 15:45:40 -0700

    ptz

--- lua/script_time_westptz.lua
+++ lua/script_time_westptz.lua
@@ -7,5 +7,12 @@ if (libs.timedifference(otherdevices_lastupdate['West PTZ']) > 600) then
 	commandArray['West PTZ']='Set Level ' .. uservariables["WestPTZ-IdlePreset"]
 end
 
+-- Change Idle Preset for Day/Night
+if (timeofday['Nighttime']) and (tonumber(uservariables["WestPTZ-IdlePreset"]) ~= 60) and (tonumber(uservariables["away"]) < 1) then
+	commandArray["Variable:WestPTZ-IdlePreset"]="60"
+elseif (timeofday['Daytime']) and (tonumber(uservariables["WestPTZ-IdlePreset"]) ~= 10) then
+	commandArray["Variable:WestPTZ-IdlePreset"]="10"
+end
+
 return commandArray
 
